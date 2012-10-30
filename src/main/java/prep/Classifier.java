75940d081c508b17ec20c7860788c8280ae3b5cf
Rouan Wilsenach <rouanw@gmail.com>
2012-10-30 21:57:32 +0200

    Daniel & Rouan : we can determine factors

--- src/main/java/prep/Classifier.java
+++ src/main/java/prep/Classifier.java
@@ -1,5 +1,9 @@
 package prep;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Classifier {
   private int number;
 
@@ -9,5 +13,15 @@ public class Classifier {
 
   public boolean hasFactor(int divisor) {
     return number % divisor == 0;
+  }
+
+  public List<Integer> factors() {
+    List<Integer> factors = new ArrayList<Integer>();
+    for (int i = 1; i <= number; i++) {
+      if (hasFactor(i)) {
+        factors.add(i);
+      }
+    }
+    return factors;
   }
 }
--- src/test/java/prep/ClassifierTest.java
+++ src/test/java/prep/ClassifierTest.java
@@ -1,7 +1,12 @@
 package prep;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 public class ClassifierTest {
@@ -13,5 +18,19 @@ public class ClassifierTest {
   @Test
   public void testKnowsWhenANumberIsNotAFactor() throws Exception {
     assertFalse(new Classifier(7).hasFactor(2));
+  }
+
+  @Test
+  public void testKnowsTheFactorsOfANumber() throws Exception {
+    List<Integer> actualFactors = new Classifier(16).factors();
+    List<Integer> expectedFactors = Arrays.asList(1, 2, 4, 8, 16);
+    assertEquals(expectedFactors, actualFactors);
+  }
+
+  @Test
+  public void testKnowsNotAllNumbersHaveTheSameFactors() throws Exception {
+    List<Integer> factorsOne = new Classifier(16).factors();
+    List<Integer> factorsTwo = new Classifier(5).factors();
+    assertThat(factorsOne, not(equalTo(factorsTwo)));
   }
 }
